62426e34197085dc33b6b4d693479d1ad203231f
stefania120102 <stefffy2014gmail.com>
2020-02-17 12:24:38 +0100

    añadir

--- src/test/java/banco/TestCuentaBancaria.java
+++ src/test/java/banco/TestCuentaBancaria.java
@@ -1,9 +1,7 @@
 package banco;
 
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
